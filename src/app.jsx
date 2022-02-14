16b4e5d8b40372c2fec209daaab311c91da45781
BokyungJeon <jjewell@naver.com>
2022-02-14 23:08:09 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './app.module.css';
 import styles from './app.module.css';
-import Search_header from './components/search_header/search_header';
+import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 import VideoList from './components/video_list/video_list';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
   const search = (query) => {
     youtube
       .search(query) //
-      .then((videos) => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   };
 
   // useEffect(): 컴포넌트가 마운트되었거나 업데이트 되었을 때마다 콜백함수를 호출
@@ -22,8 +31,17 @@ function App({ youtube }) {
 
   return (
     <div className={styles.app}>
-      <Search_header onSearch={search} />
-      <VideoList videos={videos} />
+      <SearchHeader onSearch={search} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'} />
+        </div>
+      </section>
     </div>
   );
 }
