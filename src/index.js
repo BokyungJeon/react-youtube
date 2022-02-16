15fac21cb7ee19b161421d023cb41a9ad4dca9ee
BokyungJeon <jjewell@naver.com>
2022-02-17 00:07:31 +0900

    fetch to axios

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 import Youtube from './service/youtube';
+import axios from 'axios';
+
+const httpClient = axios.create({
+  baseURL: 'https://youtube.googleapis.com/youtube/v3',
+  params: { key: process.env.REACT_APP_YOUTUBE_API_KEY },
+});
 
 // 새로운 라이브러리 같은 것 추가했을 때 안되면 재시작할 것
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const youtube = new Youtube(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
